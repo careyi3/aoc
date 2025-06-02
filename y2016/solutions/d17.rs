f44b80f236db346dece0329463668d66d9032c23
careyi3 <ian@iancarey.ie>
2025-06-02 11:57:52 +0100

    2016 Day 17

---
+++ y2016/solutions/d17.rs
@@ -0,0 +1,95 @@
+use md5::compute;
+use std::collections::HashSet;
+
+use utils::{file_reader, harness::Solve};
+
+pub struct D17;
+
+impl Solve for D17 {
+    fn part1(_input: String, path: &String) -> String {
+        let inputs = file_reader::read_lines(path);
+        let password = inputs.first().unwrap().clone();
+
+        let mut paths: Vec<String> = vec![];
+        let mut cache: HashSet<String> = HashSet::new();
+
+        walk(&password, 0, 0, &"".to_string(), &mut paths, &mut cache);
+
+        paths.sort_by(|x, y| x.len().cmp(&y.len()));
+
+        let shortest = paths.first().unwrap();
+
+        return shortest.to_string();
+    }
+
+    fn part2(_input: String, path: &String) -> String {
+        let inputs = file_reader::read_lines(path);
+        let password = inputs.first().unwrap().clone();
+
+        let mut paths: Vec<String> = vec![];
+        let mut cache: HashSet<String> = HashSet::new();
+
+        walk(&password, 0, 0, &"".to_string(), &mut paths, &mut cache);
+
+        paths.sort_by(|x, y| x.len().cmp(&y.len()));
+
+        let longest: String = paths
+            .iter()
+            .rev()
+            .collect::<Vec<&String>>()
+            .first()
+            .unwrap()
+            .to_string();
+
+        return longest.len().to_string();
+    }
+}
+
+fn walk(
+    password: &String,
+    x: i32,
+    y: i32,
+    path: &String,
+    paths: &mut Vec<String>,
+    cache: &mut HashSet<String>,
+) {
+    if cache.contains(path) {
+        return;
+    }
+    cache.insert(path.clone());
+    if x == 3 && y == 3 {
+        paths.push(path.clone());
+        return;
+    }
+    let dir_chars = ['U', 'D', 'L', 'R'];
+    for (dir, (new_x, new_y)) in [(0, -1), (0, 1), (-1, 0), (1, 0)].iter().enumerate() {
+        if is_open(x, y, &password, &path, dir) {
+            let mut new_path = path.clone();
+            new_path.push(dir_chars[dir]);
+            walk(password, x + *new_x, y + *new_y, &new_path, paths, cache);
+        }
+    }
+}
+
+fn is_open(x: i32, y: i32, password: &String, path: &String, dir: usize) -> bool {
+    if x < 0 || y < 0 {
+        return false;
+    }
+    if x > 3 || y > 3 {
+        return false;
+    }
+    let hash = hash(password, path);
+    let hash_chars: Vec<char> = hash.chars().collect();
+    let dir_char = hash_chars[dir];
+    if ['b', 'c', 'd', 'e', 'f'].contains(&dir_char) {
+        return true;
+    }
+    return false;
+}
+
+fn hash(password: &String, path: &String) -> String {
+    let message = format!("{}{}", password, path);
+    let digest = compute(message.clone());
+    let hex_digest = format!("{:x}", digest);
+    return hex_digest;
+}
